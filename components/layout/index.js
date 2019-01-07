8b4549b815479cdca3338ce1862d722edcf80471
lee <leeping88901024@gmail.com>
2019-01-07 21:54:27 +0800

    bulma

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import 'bulma/css/bulma.css'
+// import 'bulma/css/bulma.css'
 
 export default ({ children, title = 'This is the default title' }) => (
   <div>
@@ -8,23 +8,67 @@ export default ({ children, title = 'This is the default title' }) => (
       <title>{ title }</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" />
     </Head>
     <header>
       <nav>
         <Link href='/'><a>主页</a></Link> |
         <Link href='/about'><a>关于我</a></Link> |
-        <Link href='/contact'><a>联系我</a></Link>
+        <Link href='/contact'><a>联系我</a></Link> |
+        <Link href='/login'><a>登录</a></Link>
+      </nav>
+      <nav className='navbar has-shadow'>
+        <div className='navbar-brand'>
+          <a className='navbar-item'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyzzFrb4FLMHRZtqshSqoTdOk75ziPTviKoLhZuDGzDSem56fdiQ' />
+          </a>
+        </div>
+        <div className='navbar-menu'>
+          <div className='navbar-start'>
+            <div className='navbar-item'>
+              <small>发布技术博文</small>
+            </div>
+          </div>
+          <div className='navbar-end'>
+            <div className='navbar-item has-dropdown is-hoverable'>
+              <div className='navbar-link'>
+                是我
+              </div>
+              <div className='navbar-dropdown'>
+                <a className='navbar-item'>
+                  <div>
+                    <span className='icon is-small'>
+                      <i className='fa fa-user-circle-o'></i>
+                    </span>
+                    自我介绍
+                  </div>
+                </a>
+                <a className='navbar-item'>
+                  <div>
+                    <span className='icon is-small'>
+                      <i className='fa fa-bug'></i>
+                    </span>
+                    项目报告
+                  </div>
+                </a>
+                <a className='navbar-item'>
+                  <div>
+                    <span className='icon is-small'>
+                      <i className='fa fa-sign-out'></i>
+                    </span>
+                    退出
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
 
     { children }
 
-    <a className="button is-primary">Primary</a>
-    <a className="button is-link">Link</a>
-    <a className="button is-info">Info</a>
-    <a className="button is-success">Success</a>
-    <a className="button is-warning">Warning</a>
-    <a className="button is-danger">Danger</a>
     <footer>
       {'我要待在这里'}
     </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
 import Layout from '../components/layout'
+// import 'bulma/css/bulma.css'
 
 export default () => (
   <Layout>
-    <div>你好 nenxt.js。我将构建响应速度极快的博客</div>
+      <div className='columns'>
+          {/* 第一列 */}
+          <div className='column'></div>
+          {/* 第二列 */}
+          <div className='column'></div>
+          {/* 第三列 */}
+          <div className='column'></div>
+
+          {/* 按钮 */}
+          <a className='button is-primary is-large is-outlined'>
+              保存更改
+          </a>
+
+          {/* 组件 */}
+          <div className='card'>
+              <header className='card-header'>
+                  这是头
+              </header>
+
+              <div className='card-content'>
+                  <div className='card-image'>
+                      <img src='https://bulma.io/images/css-book/css-in-44-minutes-book-cover.png' />
+                  </div>
+              </div>
+
+              <footer className='card-footer'>
+              hhhhhhh
+              </footer>
+          </div>
+      </div>
+      <div>到第四十页面</div>
   </Layout>
 )
