6695e584c07679601a35ec17086e9f2111319e39
lee <leeping88901024@gmail.com>
2019-01-08 17:38:16 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css" />
-          <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
         </Head>
         <body className="custom_class">
           <Main />
