e5632995d2f3edcef1f2765cd106524a719bc964
lee <leeping88901024@gmail.com>
2019-01-09 16:59:20 +0800

    change color

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import MultiboxMenu from '../components/MultiboxMenu'
 
 export default () => (
   <MultiboxMenu title='主页'>
-    {/* 
-    
-    <p className="content__tagline">有关内容的标签</p>   
-    <h2 className="content__title">内容标题</h2>*/}
-    
+    <div>
+      hhhhhhhhhh
+    </div>
   </MultiboxMenu>
 )
